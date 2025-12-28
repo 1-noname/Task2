6fa1fd74c35e7896ba76e068b1e7f956550b3b33
NoName <mostblack4@gmail.com>
2025-12-28 23:51:15 +0300

    refactor: remove console.log from webpack config

--- config/build/buildWebpackConfig.ts
+++ config/build/buildWebpackConfig.ts
@@ -10,7 +10,7 @@ export function buildWebpackConfig(
   options: BuildOptions,
 ): webpack.Configuration {
   const { paths, mode, isDev } = options;
-  console.log("LOADERS", buildLoaders(options));
+
   return {
     mode,
     entry: paths.entry,
