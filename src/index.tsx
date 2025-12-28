9880c929e59e00580c3ae6dd5cb20db16f2fe1e1
NoName <mostblack4@gmail.com>
2025-12-28 23:57:22 +0300

    feat(index): add browser router and store provider

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+
+import App from "./app/App";
+import { StoreProvider } from "./app/providers/StoreProvider";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <StoreProvider>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StoreProvider>,
+);
