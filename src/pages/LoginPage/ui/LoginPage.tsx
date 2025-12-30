8c262ecd05e60cc678ff1ff1cd8c8277bbdaf26f
NoName <mostblack4@gmail.com>
2025-12-31 02:21:06 +0300

    refactor(LoginPage): cleared page component

--- src/pages/LoginPage/ui/LoginPage.tsx
+++ src/pages/LoginPage/ui/LoginPage.tsx
@@ -1,25 +1,8 @@
-import { ChangeEvent, useState } from "react";
+import { LoginForm } from "@/features/auth";
 
 import cls from "./LoginPage.module.scss";
 
-import LockIcon from "@mui/icons-material/Lock";
-import PersonIcon from "@mui/icons-material/Person";
-import { Button, InputAdornment, TextField } from "@mui/material";
-
 export const LoginPage = () => {
-  const [login, setLogin] = useState("");
-  const [password, setPassword] = useState("");
-
-  const isFilled = login !== "" && password !== "";
-
-  const handleLogin = (e: ChangeEvent<HTMLInputElement>) => {
-    setLogin(e.target.value);
-  };
-
-  const handlePassword = (e: ChangeEvent<HTMLInputElement>) => {
-    setPassword(e.target.value);
-  };
-
   return (
     <div className={cls.login}>
       <div className={cls.loginContainer}>
@@ -28,93 +11,7 @@ export const LoginPage = () => {
           <div className={cls.loginDesc}>
             Введите имя пользователя и пароль, чтобы войти в свою учетную запись
           </div>
-        </div>
-        <form className={cls.loginForm}>
-          <TextField
-            label="Логин"
-            variant="outlined"
-            value={login}
-            onChange={handleLogin}
-            slotProps={{
-              input: {
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <PersonIcon sx={{ color: "var(--color-primary)" }} />
-                  </InputAdornment>
-                ),
-              },
-            }}
-            sx={{
-              "& .MuiOutlinedInput-root": {
-                "& fieldset": {
-                  borderColor: "var(--color-primary)",
-                },
-                "&:hover fieldset": {
-                  borderColor: "var(--hover-primary)",
-                },
-                "&.Mui-focused fieldset": {
-                  borderColor: "var(--red)",
-                },
-              },
-              "& .MuiInputLabel-root": {
-                color: "var(--color-primary)",
-              },
-              "& .MuiInputLabel-root.Mui-focused": {
-                color: "var(--red)",
-              },
-            }}
-          />
-          <TextField
-            label="Пароль"
-            variant="outlined"
-            value={password}
-            onChange={handlePassword}
-            slotProps={{
-              input: {
-                startAdornment: (
-                  <InputAdornment position="start">
-                    <LockIcon sx={{ color: "var(--color-primary)" }} />
-                  </InputAdornment>
-                ),
-              },
-            }}
-            sx={{
-              "& .MuiOutlinedInput-root": {
-                "& fieldset": {
-                  borderColor: "var(--color-primary)",
-                },
-                "&:hover fieldset": {
-                  borderColor: "var(--hover-primary)",
-                },
-                "&.Mui-focused fieldset": {
-                  borderColor: "var(--red)",
-                },
-              },
-              "& .MuiInputLabel-root": {
-                color: "var(--color-primary)",
-              },
-              "& .MuiInputLabel-root.Mui-focused": {
-                color: "var(--red)",
-              },
-            }}
-          />
-        </form>
-        <div>
-          <Button
-            variant="contained"
-            disabled={!isFilled}
-            sx={{
-              "&.Mui-disabled": {
-                color: "var(--color-primary)",
-                background: "var(--button-inactive)",
-              },
-              width: "100%",
-              color: "var(--color-secondary)",
-              background: "var(--button-active)",
-            }}
-          >
-            Авторизация
-          </Button>
+          <LoginForm />
         </div>
       </div>
     </div>
