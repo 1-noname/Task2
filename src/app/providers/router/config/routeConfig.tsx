b5fd2f23e2dd22a3542a3a325b9754a7f977c351
NoName <mostblack4@gmail.com>
2025-12-31 20:26:02 +0300

    feat: add new path for test guards

--- src/app/providers/router/config/routeConfig.tsx
+++ src/app/providers/router/config/routeConfig.tsx
@@ -3,23 +3,38 @@ import { RouteProps } from "react-router-dom";
 import { LoginPage } from "@/pages/LoginPage";
 import { NotFound } from "@/pages/NotFound";
 
+export type AppRouteProps = RouteProps & {
+  authOnly?: boolean;
+};
+
 enum AppRoutes {
   LOGIN = "login",
   NOT_FOUND = "not_found",
+  MAIN = "main",
 }
 
 const routePaths: Record<AppRoutes, string> = {
   [AppRoutes.LOGIN]: "/login",
   [AppRoutes.NOT_FOUND]: "*",
+  [AppRoutes.MAIN]: "/",
 };
 
-export const routeConfig: Record<AppRoutes, RouteProps> = {
+export const routeConfig: Record<AppRoutes, AppRouteProps> = {
   [AppRoutes.LOGIN]: {
     path: routePaths.login,
     element: <LoginPage />,
+    authOnly: false,
   },
+
   [AppRoutes.NOT_FOUND]: {
     path: routePaths.not_found,
     element: <NotFound />,
+    authOnly: false,
+  },
+
+  [AppRoutes.MAIN]: {
+    path: routePaths.main,
+    element: <div>haloooo</div>,
+    authOnly: true,
   },
 };
