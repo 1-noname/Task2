29d9136a6bcaf77d5be4a5545acfc8b54a9fad8b
NoName <mostblack4@gmail.com>
2026-01-05 23:20:29 +0300

    feat(useProductsList): create hook for lazy loading products in list

---
+++ src/features/products/model/hooks/useProductsList.tsx
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { useInView } from "react-intersection-observer";
+
+import { LIMIT } from "@/entities/product/model/constants";
+
+import { useGetProductsQuery } from "../../api/productsApi";
+
+export const useProductsList = () => {
+  const [skip, setSkip] = useState(0);
+
+  const { ref, inView } = useInView({
+    threshold: 0.5,
+    rootMargin: "100px",
+  });
+
+  const { data, isLoading, isError, isFetching } = useGetProductsQuery({
+    limit: LIMIT,
+    skip,
+  });
+
+  useEffect(() => {
+    const hasScroll = data && data.products.length < data.total;
+    if (inView && !isLoading && hasScroll) {
+      setSkip((prev) => prev + LIMIT);
+    }
+  }, [inView, isFetching, data]);
+
+  return {
+    products: data?.products,
+    isLoading,
+    isError,
+    trigger: ref,
+  };
+};
