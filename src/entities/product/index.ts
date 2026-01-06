30086891a0c8822392db5a34c65a608b652581fc
NoName <mostblack4@gmail.com>
2026-01-06 17:47:01 +0300

    refactor: add products api to entites layer with types directory

--- src/entities/product/index.ts
+++ src/entities/product/index.ts
@@ -1,1 +1,8 @@
+export {
+  useAddProductMutation,
+  useDeleteProductMutation,
+  useGetProductsQuery,
+  useUpdateProductMutation,
+} from "./api/productsApi";
+export { LIMIT } from "./model/constants/constants";
 export { ProductCard } from "./ui/ProductCard";
---
+++ src/entities/product/model/types/types.ts
@@ -0,0 +1,21 @@
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  rating: number;
+  stock: number;
+}
+
+export interface ProductsResponse {
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
+}
+
+export interface ProductsLimit {
+  limit: number;
+  skip: number;
+}
