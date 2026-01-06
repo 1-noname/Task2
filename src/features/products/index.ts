a6f4bd2599c499839b520f62bf1ab788e274e45f
NoName <mostblack4@gmail.com>
2026-01-06 17:46:06 +0300

    refactor: remove product from features layer

--- src/features/products/index.ts
+++
@@ -1,7 +0,0 @@
-export {
-  useAddProductMutation,
-  useDeleteProductMutation,
-  useGetProductsQuery,
-  useUpdateProductMutation,
-} from "./api/productsApi";
-export { ProductsList } from "./ui/ProductsList";
