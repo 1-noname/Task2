67f10e157b1aa9bfc2cdefcc4600a26f6c1221f9
NoName <mostblack4@gmail.com>
2026-01-08 21:00:45 +0300

    refactor(ProductCardSkeleton): update background and add animations

--- src/shared/ui/ProductCardSkeleton/ProductCardSkeleton.tsx
+++ src/shared/ui/ProductCardSkeleton/ProductCardSkeleton.tsx
@@ -5,16 +5,16 @@ import { Skeleton } from "@mui/material";
 export const ProductCardSkeleton = () => {
   return (
     <div className={cls.card}>
-      <Skeleton variant="rounded" width="100%" height={180} />
+      <Skeleton animation="wave" variant="rounded" width="100%" height={180} />
 
       <div className={cls.content}>
-        <Skeleton height={28} width="70%" />
+        <Skeleton animation="wave" height={28} width="70%" />
 
-        <Skeleton height={18} width="100%" />
-        <Skeleton height={18} width="90%" />
+        <Skeleton animation="wave" height={18} width="100%" />
+        <Skeleton animation="wave" height={18} width="90%" />
 
         <div className={cls.footer}>
-          <Skeleton height={24} width={80} />
+          <Skeleton animation="wave" height={24} width={80} />
 
           <Skeleton variant="rounded" height={36} width={36} />
         </div>
