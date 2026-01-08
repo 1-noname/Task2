3e7a467121cdcdf503821eb4d8444d9478d8b7cf
NoName <mostblack4@gmail.com>
2026-01-08 04:21:06 +0300

    feat(ProductList): add card skeleton

--- src/widgets/productsList/ui/ProductsList.tsx
+++ src/widgets/productsList/ui/ProductsList.tsx
@@ -1,6 +1,6 @@
 import { ProductCard } from "@/entities/product";
 import { DeleteProduct } from "@/features/deleteProduct";
-import { PageLoader } from "@/shared/ui/PageLoader";
+import { ProductCardSkeleton } from "@/shared/ui";
 
 import { useLazyDataFetching } from "../model/hooks/useLazyDataFetching";
 import cls from "./ProductsList.module.scss";
@@ -8,29 +8,29 @@ import cls from "./ProductsList.module.scss";
 export const ProductsList = () => {
   const { products, isLoading, isError, trigger } = useLazyDataFetching();
 
-  if (isLoading) {
-    return <PageLoader />;
-  }
-
   if (isError) {
     return <div>:(</div>;
   }
 
   return (
     <div className={cls.products}>
-      {products.map(
-        ({ id, title, description, category, thumbnail, price }) => (
-          <ProductCard
-            key={id}
-            id={id}
-            title={title}
-            description={description}
-            category={category}
-            thumbnail={thumbnail}
-            price={price}
-            deleteButton={<DeleteProduct id={id} />}
-          />
-        ),
+      {isLoading ? (
+        <ProductCardSkeleton />
+      ) : (
+        products.map(
+          ({ id, title, description, category, thumbnail, price }) => (
+            <ProductCard
+              key={id}
+              id={id}
+              title={title}
+              description={description}
+              category={category}
+              thumbnail={thumbnail}
+              price={price}
+              deleteButton={<DeleteProduct id={id} />}
+            />
+          ),
+        )
       )}
       <div ref={trigger}></div>
     </div>
