ffa4b3abf6029b3f4cab7f5e9e5e8bbc354de611
NoName <mostblack4@gmail.com>
2026-01-08 20:38:04 +0300

    fix: re-export types

--- src/features/auth/index.ts
+++ src/features/auth/index.ts
@@ -1,3 +1,3 @@
 export { useLoginMutation } from "./api/authApi";
-export { LoginResponse } from "./model/types/types";
+export type { LoginResponse } from "./model/types/types";
 export { LoginForm } from "./ui/LoginForm";
