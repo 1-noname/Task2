31d178c059625ae6abc16e273cac4a58114e86da
NoName <mostblack4@gmail.com>
2026-01-08 03:45:45 +0300

    feat(useLazyDataFetching): reset pagination on search change

--- src/widgets/productsList/model/hooks/useLazyDataFetching.tsx
+++ src/widgets/productsList/model/hooks/useLazyDataFetching.tsx
@@ -2,11 +2,13 @@ import { useEffect, useState } from "react";
 import { useInView } from "react-intersection-observer";
 
 import { LIMIT } from "@/entities/product";
-
-import { useGetProductsQuery } from "../../api/productsApi";
+import { useGetProductsQuery } from "@/entities/product";
+import { selectProductName } from "@/features/searchProduct/model/slice/searchSlice";
+import { useAppSelector } from "@/shared/lib/hooks/useAppSelector";
 
 export const useLazyDataFetching = () => {
   const [skip, setSkip] = useState(0);
+  const search = useAppSelector(selectProductName);
   const { ref, inView } = useInView({
     threshold: 0.5,
     rootMargin: "100px",
@@ -14,7 +16,12 @@ export const useLazyDataFetching = () => {
   const { data, isLoading, isError, isFetching } = useGetProductsQuery({
     limit: LIMIT,
     skip,
+    search,
   });
+
+  useEffect(() => {
+    setSkip(0);
+  }, [search]);
 
   useEffect(() => {
     const hasScroll = data && data.products.length < data.total;
