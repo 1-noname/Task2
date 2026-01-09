eaaf0181ed591daba85e8e7519a1488305833c4c
NoName <mostblack4@gmail.com>
2026-01-09 15:53:16 +0300

    feat(useHeader): add ui state & handlers for user menu and handler for logout

--- src/widgets/header/model/hooks/useHeader.tsx
+++ src/widgets/header/model/hooks/useHeader.tsx
@@ -1,10 +1,22 @@
 import { MouseEvent, useState } from "react";
 
-import { useMeQuery } from "@/entities/session";
+import {
+  logout,
+  selectIsAuth,
+  selectUser,
+  useMeQuery,
+} from "@/entities/session";
+import { useAppDispatch, useAppSelector } from "@/shared/lib";
 
 export const useHeader = () => {
-  const { data: user, isLoading } = useMeQuery();
+  const dispatch = useAppDispatch();
+  const { isLoading } = useMeQuery();
+
+  const user = useAppSelector(selectUser);
+  const isAuth = useAppSelector(selectIsAuth);
+
   const [anchorElNav, setAnchorElNav] = useState<null | HTMLElement>(null);
+  const [anchorElUser, setAnchorElUser] = useState<null | HTMLElement>(null);
 
   const handleOpenNavMenu = (event: MouseEvent<HTMLElement>) => {
     setAnchorElNav(event.currentTarget);
@@ -14,11 +26,29 @@ export const useHeader = () => {
     setAnchorElNav(null);
   };
 
+  const handleOpenUserMenu = (event: MouseEvent<HTMLElement>) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  const handleLogout = () => {
+    dispatch(logout());
+    handleCloseUserMenu();
+  };
+
   return {
     user,
+    isAuth,
     isLoading,
     anchorElNav,
+    anchorElUser,
     handleOpenNavMenu,
     handleCloseNavMenu,
+    handleOpenUserMenu,
+    handleCloseUserMenu,
+    handleLogout,
   };
 };
