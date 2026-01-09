9b49708444ec4c305874310d96ecfa244433bd0a
NoName <mostblack4@gmail.com>
2026-01-09 16:45:41 +0300

    fix(App): add condition to hidden header when user on login page

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,11 +1,18 @@
+import { useLocation } from "react-router-dom";
+
 import { Header } from "@/widgets/header";
 
 import { Router } from "./providers/router";
 
 const App = () => {
+  const { pathname } = useLocation();
+
+  const hideHeader = ["/login"];
+  const isHeaderVisible = !hideHeader.includes(pathname);
+
   return (
     <>
-      <Header />
+      {isHeaderVisible ? <Header /> : null}
       <main className="app">
         <Router />
       </main>
