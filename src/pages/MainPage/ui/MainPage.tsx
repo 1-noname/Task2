9bfae9e2919c2dfaf8f0a955d1d39a6fd1883a60
NoName <mostblack4@gmail.com>
2026-01-09 04:11:52 +0300

    feat(MainPage): add state to work with UI

--- src/pages/MainPage/ui/MainPage.tsx
+++ src/pages/MainPage/ui/MainPage.tsx
@@ -1,11 +1,32 @@
+import { useState } from "react";
+
 import { SearchProduct } from "@/features/searchProduct/ui/SearchProduct";
+import { ProductsFilter } from "@/widgets/productsFilter";
 import { ProductsList } from "@/widgets/productsList";
 
+import cls from "./MainPage.module.scss";
+
 export const MainPage = () => {
+  const [isFilterOpen, setIsFilterOpen] = useState(true);
+
+  const handleFilterOpen = () => {
+    setIsFilterOpen((prev) => !prev);
+  };
+
   return (
-    <div>
-      <SearchProduct isOpen={false} />
-      <ProductsList />
-    </div>
+    <>
+      <div>
+        <SearchProduct
+          isFilterOpen={isFilterOpen}
+          onToggle={handleFilterOpen}
+        />
+      </div>
+      <div
+        className={`${cls.content} ${isFilterOpen ? cls.withFilter : cls.noFilter}`}
+      >
+        <ProductsList />
+        {isFilterOpen && <ProductsFilter />}
+      </div>
+    </>
   );
 };
