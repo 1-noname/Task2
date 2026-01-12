12c279586d5291fed60a7b1b4c5a321a7bcb9ee4
NoName <mostblack4@gmail.com>
2026-01-12 03:20:32 +0300

    feat(ProductFilter): add button to create products

--- src/widgets/productsFilter/ui/ProductsFilter.tsx
+++ src/widgets/productsFilter/ui/ProductsFilter.tsx
@@ -1,11 +1,17 @@
+import { selectIsAdmin } from "@/entities/session";
+import { AddProduct } from "@/features/addProduct";
 import { FilterCategory } from "@/features/filterCategory";
+import { useAppSelector } from "@/shared/lib";
 
 import cls from "./ProductsFilter.module.scss";
 
 export const ProductsFilter = () => {
+  const isAdmin = useAppSelector(selectIsAdmin);
+
   return (
     <div className={cls.filter}>
       <FilterCategory />
+      {isAdmin && <AddProduct />}
     </div>
   );
 };
