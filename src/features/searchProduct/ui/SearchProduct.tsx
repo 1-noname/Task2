a83d296acbb79a6ccfd4f83e6659e0846381fced
NoName <mostblack4@gmail.com>
2026-01-13 04:36:12 +0300

    fix(SearchProduct): clearing and disabling input when category set

--- src/features/searchProduct/ui/SearchProduct.tsx
+++ src/features/searchProduct/ui/SearchProduct.tsx
@@ -1,4 +1,9 @@
-import { useSearchProduct } from "../model/hooks/useSearchProduct";
+import { ChangeEvent, useEffect } from "react";
+
+import { selectCategory } from "@/features/filterCategory/model/slice/categorySlice";
+import { useAppDispatch, useAppSelector } from "@/shared/lib";
+
+import { selectProductName, setSearch } from "../model/slice/searchSlice";
 import cls from "./SearchProducts.module.scss";
 
 import FilterAltIcon from "@mui/icons-material/FilterAlt";
@@ -13,14 +18,33 @@ export const SearchProduct = ({
   isFilterOpen,
   onToggle,
 }: SearchProductProps) => {
-  const { handleChange } = useSearchProduct();
+  const dispatch = useAppDispatch();
+
+  const productName = useAppSelector(selectProductName);
+  const selectedCategory = useAppSelector(selectCategory);
+
+  const isSearchDisabled = Boolean(selectedCategory);
+
+  useEffect(() => {
+    if (selectedCategory) {
+      dispatch(setSearch(""));
+    }
+  }, [selectedCategory, dispatch]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    dispatch(setSearch(e.target.value));
+  };
 
   return (
     <div className={cls.search}>
       <input
-        placeholder="Type name here..."
-        className={cls.input}
+        value={productName}
         onChange={handleChange}
+        placeholder={
+          isSearchDisabled ? "Clear category to search..." : "Type name here..."
+        }
+        className={cls.input}
+        disabled={isSearchDisabled}
       />
       <button className={cls.button} onClick={onToggle}>
         {isFilterOpen ? <FilterAltOffIcon /> : <FilterAltIcon />}
